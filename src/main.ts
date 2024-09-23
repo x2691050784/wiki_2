95ec0dbfdd540cdb2e053504abdd5ee57b504409
x2691050784 <2691050784@qq.com>
2024-09-23 10:22:25 +0800

    完成接口调用

--- src/main.ts
+++ src/main.ts
@@ -4,4 +4,35 @@ import router from './router'
 import store from './store'
 import Antd from 'ant-design-vue';
 import 'ant-design-vue/dist/antd.css';
-createApp(App).use(store).use(router).use(Antd).mount('#app')
+import * as Icons from '@ant-design/icons-vue';
+import axios from 'axios';
+axios.defaults.baseURL = process.env.VUE_APP_SERVER;
+
+/**
+ * axios拦截器
+ */
+axios.interceptors.request.use(function (config) {
+    console.log('请求参数：', config);
+    return config;
+}, error => {
+    return Promise.reject(error);
+});
+axios.interceptors.response.use(function (response) {
+    console.log('返回结果：', response);
+    return response;
+}, error => {
+    console.log('返回错误：', error);
+    return Promise.reject(error);
+});
+
+const app=createApp(App);
+app.use(router);
+app.use(store);
+app.use(Antd).mount('#app');
+ const icons: any= Icons;
+for (const iconsKey in icons) {
+  app.component(iconsKey, icons[iconsKey]);
+}
+
+console.log('环境：', process.env.NODE_ENV);
+console.log('服务端：', process.env.VUE_APP_SERVER);
---
+++ src/views/admin/1.vue
@@ -0,0 +1,15 @@
+<script lang="ts">
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+  name: "1"
+})
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
