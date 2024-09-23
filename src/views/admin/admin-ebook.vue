c439df5ed2defda7dcfc66c441b5eabf4c1b2bbc
x2691050784 <2691050784@qq.com>
2024-09-23 20:20:26 +0800

    改变电子书布局

---
+++ src/views/admin/admin-ebook.vue
@@ -0,0 +1,17 @@
+<template>
+
+    <a-layout>
+    <a-layout-content
+        :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
+    >
+      <div class="about">
+        <h1>
+          admin-ebook
+        </h1>
+      </div>
+    </a-layout-content>
+    </a-layout>
+
+</template>
+<script setup lang="ts">
+</script>
